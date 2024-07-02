5645123cfbfb6667f1ddbcd71e4b4236eb1666f0
Anuradha Naidu <anuradhaknaidu6@gmail.com>
2024-07-02 22:05:02 +0530

    final steps

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PostList from './components/PostList';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+        {/* Component for home page */}
+        </Route>
+        <Route path="/about">
+      {/* Component for about page */}
+        </Route>
+    {/* Add more routes as needed */}
+      </Switch>
+    </Router>
+    
   );
 }
 
 export default App;
+
